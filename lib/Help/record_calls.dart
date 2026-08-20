16f50f745f18ec3878fe7af5773f1d46364ed947
unknown <mdmehedidesktop@gmail.com>
2026-08-20 17:31:07 +0600

    Update

--- lib/Help/record_calls.dart
+++ lib/Help/record_calls.dart
@@ -10,6 +10,7 @@ class _RecordCallsState extends State<RecordCalls> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0Xff303030),
       appBar: AppBar(
         backgroundColor: Color(0Xff1C1C1C),
         title: Text("Help",style: TextStyle(fontSize: 20,color: Colors.white),),
@@ -17,8 +18,16 @@ class _RecordCallsState extends State<RecordCalls> {
         Icon(Icons.close,color: Colors.white,),
         SizedBox(width: 10,),
         Icon(Icons.more_vert,color: Colors.white,),SizedBox(width: 10,),
-
       ],
+      ),
+      body: Padding(padding: EdgeInsets.all(13),
+        child: Column(
+          children: [
+            Text("Use the Phone app to\nrecord calls",style: TextStyle(fontSize: 25,color: Colors.white),)
+          ],
+        ),
+
+
       ),
     );
   }
