57376acc021f40174575285e8c3e09ce84557281
unknown <mdmehedidesktop@gmail.com>
2026-08-22 18:53:13 +0600

    Update

--- lib/Help/record_calls.dart
+++ lib/Help/record_calls.dart
@@ -43,6 +43,7 @@ class _RecordCallsState extends State<RecordCalls> {
               Text("Important:",style: TextStyle(fontSize: 20,color: Colors.white),),
               Text(" Only certain device and",style: TextStyle(fontSize: 16,color: Colors.white),),
             ],),
+            Text("operators support recording calls .Some laws require all people on the call to consent to being , call participants are notified that the call is recorded.",style: TextStyle(fontSize: 16,color: Colors.white),)
 
           ],
         ),
