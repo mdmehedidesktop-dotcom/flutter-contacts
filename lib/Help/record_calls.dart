65718ce667bb441a3d9670b05153dbdd2571ef9f
unknown <mdmehedidesktop@gmail.com>
2026-08-23 16:01:05 +0600

    Update

--- lib/Help/record_calls.dart
+++ lib/Help/record_calls.dart
@@ -20,36 +20,42 @@ class _RecordCallsState extends State<RecordCalls> {
         Icon(Icons.more_vert,color: Colors.white,),SizedBox(width: 10,),
       ],
       ),
-      body: Padding(padding: EdgeInsets.all(13),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Use the Phone app to\nrecord calls",style: TextStyle(fontSize: 25,color: Colors.white),),
-            Text("You can use your phone app to :",style: TextStyle(fontSize: 20,color: Colors.white),),
-            SizedBox(height: 20,),
-            Text(". Always record calls from unknow\nnumber.",style: TextStyle(fontSize: 20,color: Colors.white),),
-            SizedBox(height: 12,),
-            Text(". Always record calls from selected\ncontacts.",style: TextStyle(fontSize: 2,color: Colors.white),),
-            SizedBox(height: 12,),
-            Text(". Record an individual call.",style: TextStyle(fontSize:20,color: Colors.white),),
-            SizedBox(height: 20,),
-            Text("Tips :",style: TextStyle(fontSize: 20,color: Colors.white),),
-            SizedBox(height: 10,),
-            Text(". You can play back saved recordings and also modify how to keep recorded calls.",style: TextStyle(fontSize: 16,color: Colors.white),),
-            SizedBox(height: 10,),
-            Text(". You can see when call recording is active with the call recording indicator.",style: TextStyle(fontSize: 16,color: Colors.white),),
-            SizedBox(height: 20,),
-            Row(children: [
-              Text("Important:",style: TextStyle(fontSize: 20,color: Colors.white),),
-              Text(" Only certain device and",style: TextStyle(fontSize: 16,color: Colors.white),),
-            ],),
-            Text("operators support recording calls .Some laws require all people on the call to consent to being , call participants are notified that the call is recorded.",style: TextStyle(fontSize: 16,color: Colors.white),),
-            Text("To record your phone calls :",style: TextStyle(fontSize: 18,color: Colors.white),)
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(padding: EdgeInsets.all(13),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Use the Phone app to\nrecord calls",style: TextStyle(fontSize: 25,color: Colors.white),),
+              Text("You can use your phone app to :",style: TextStyle(fontSize: 20,color: Colors.white),),
+              SizedBox(height: 20,),
+              Text(". Always record calls from unknow\nnumber.",style: TextStyle(fontSize: 20,color: Colors.white),),
+              SizedBox(height: 12,),
+              Text(". Always record calls from selected\ncontacts.",style: TextStyle(fontSize: 2,color: Colors.white),),
+              SizedBox(height: 12,),
+              Text(". Record an individual call.",style: TextStyle(fontSize:20,color: Colors.white),),
+              SizedBox(height: 20,),
+              Text("Tips :",style: TextStyle(fontSize: 20,color: Colors.white),),
+              SizedBox(height: 10,),
+              Text(". You can play back saved recordings and also modify how to keep recorded calls.",style: TextStyle(fontSize: 16,color: Colors.white),),
+              SizedBox(height: 10,),
+              Text(". You can see when call recording is active with the call recording indicator.",style: TextStyle(fontSize: 16,color: Colors.white),),
+              SizedBox(height: 20,),
+              Row(children: [
+                Text("Important:",style: TextStyle(fontSize: 20,color: Colors.white),),
+                Text(" Only certain device and",style: TextStyle(fontSize: 16,color: Colors.white),),
+              ],),
+              Text("operators support recording calls .Some laws require all people on the call to consent to being , call participants are notified that the call is recorded.",style: TextStyle(fontSize: 16,color: Colors.white),),
+              Text("To record your phone calls :",style: TextStyle(fontSize: 18,color: Colors.white),),
+              Text(". None-pixel device must run Android 9 and up. ",style: TextStyle(fontSize: 16,color: Colors.white),),
+              Text(". Pixel devices must be pixel 6 and uP with Android  14 and up. ",style: TextStyle(fontSize: 16,color: Colors.white),),
+              Text(". you mast have the latest version of the phone app. ",style: TextStyle(fontSize: 16,color: Colors.white),),
 
-          ],
+            ],
+          ),
+
+
         ),
-
-
       ),
     );
   }
